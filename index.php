4bbfdc74575c43d85ed1952b817bfd8b1e0e62c6
Bayu Firmawan Paoh <bayupaoh@gmail.com>
2020-05-03 15:08:03 +0700

    Update from parent 2

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 echo "<h3> Hello Bayu </h3>";
 echo "<h3> Hello Test Conflict </h3>";
 echo "<h3> Update from <a href="https://github.com/bayupaoh/learn-git-2018/edit/master/index.php">here</a> </h3>";
+echo "<h3> Update from <a href="https://github.com/bayupaoh/learn-git-2018/edit/master/index.php">here</a> </h3>";
 ?>
